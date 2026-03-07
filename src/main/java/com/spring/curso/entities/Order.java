e1a395b8e130b1c18fdb0cf95ed954a5914b5f17
Carlos-Eduardo-Bs <eduardo20power@gmail.com>
2026-03-06 22:15:43 -0300

    feat: adicionada a entidade Payment e associação one-to-one com Order

--- src/main/java/com/spring/curso/entities/Order.java
+++ src/main/java/com/spring/curso/entities/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.spring.curso.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,14 +16,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -39,7 +41,10 @@ public class Order implements Serializable{
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
-    public Order(){
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order() {
     }
 
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
@@ -69,7 +74,7 @@ public class Order implements Serializable{
         return OrderStatus.valueOf(orderStatus);
     }
 
-    public void setOrderStatus(OrderStatus orderStatus){
+    public void setOrderStatus(OrderStatus orderStatus) {
         if (orderStatus != null) {
             this.orderStatus = orderStatus.getCode();
         }
@@ -83,8 +88,16 @@ public class Order implements Serializable{
         this.client = client;
     }
 
-    public Set<OrderItem> getItems(){
+    public Set<OrderItem> getItems() {
         return items;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     @Override
@@ -116,5 +129,4 @@ public class Order implements Serializable{
         return serialVersionUID;
     }
 
-    
 }
